87807c06eb832c2ce922ff9f3c75fb59cf813bda
gon2410 <goonolivera@hotmail.com>
2023-04-10 11:10:40 -0300

    some update

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -46,54 +46,57 @@ const Cart = () => {
     } else {
         return (
             <>
-                <Container className="row mt-5 justify-content-center">
-                    <Col>
-                        {
-                            cartList.map((item) => {
-                                total += item.price
-                                return (
-                                    <>
-                                        <Card key={item.id} style={{ width: '28rem' }} className="mb-2">
-                                            <Row>
-                                                <Col>
-                                                    <Card.Img variant="top" src={item.image_url} />
-                                                </Col>
-                                                <Col>
-                                                    <Card.Body>
-                                                        <Card.Text>{item.brand + " " + item.model} X{item.quantity}</Card.Text>
-                                                        <Button onClick={() => {
-                                                            setCartList(
-                                                                cartList.filter(it => it.model !== item.model)
-                                                            );
-                                                            updateStock(item);
-                                                            
-                                                        }} className="btn-sm btn-danger">Eliminar</Button>
-                                                    </Card.Body>
-                                                </Col>
-                                            </Row>
-                                        </Card>
-                                    </>
-                                )
-                            })
-                        }
-                    </Col>
-                    <Col>
-                        <Container className="border rounded p-2">
-                            <Form onSubmit={handleSubmit}>
+                <Container className="mt-5">
+                    <Row>
+                        <Col>
+                            {
+                                cartList.map((item) => {
+                                    total += item.price
+                                    return (
+                                        <>
+                                            <Card key={item.id} style={{ width: '28rem' }} className="mb-2">
+                                                <Row>
+                                                    <Col>
+                                                        <Card.Img variant="top" src={item.image_url} />
+                                                    </Col>
+                                                    <Col>
+                                                        <Card.Body>
+                                                            <Card.Text>{item.brand + " " + item.model} X{item.quantity}</Card.Text>
+                                                            <Button onClick={() => {
+                                                                setCartList(
+                                                                    cartList.filter(it => it.model !== item.model)
+                                                                );
+                                                                updateStock(item);
+                                                            }} className="btn-sm btn-light"><span class="material-symbols-outlined">delete</span></Button>
+                                                        </Card.Body>
+                                                    </Col>
+                                                </Row>
+                                            </Card>
+                                        </>
+                                    )
+                                })
+                            }
+                        </Col>
+                        <Col>
+                            <Container className="border rounded p-2">
+                                <Form onSubmit={handleSubmit}>
 
-                                <Form.Group className="mt-3">
-                                    <Form.Control type="text" placeholder="Ingrese su nombre" onChange={(e) => setName(e.target.value)}></Form.Control>
-                                </Form.Group>
+                                    <Form.Group className="mt-3">
+                                        <Form.Control type="text" placeholder="Ingrese su nombre" onChange={(e) => setName(e.target.value)}></Form.Control>
+                                    </Form.Group>
 
-                                <Form.Group className="mt-3">
-                                    <Form.Control type="email" placeholder="Ingrese su direccion de e-mail" onChange={(e) => setEmail(e.target.value)}></Form.Control>
-                                </Form.Group>
-                                <p>Total: {total}</p>
-                                <Button className="mt-3" variant="primary" type="submit">Comprar</Button>
-                            </Form>
+                                    <Form.Group className="mt-3">
+                                        <Form.Control type="email" placeholder="Ingrese su direccion de e-mail" onChange={(e) => setEmail(e.target.value)}></Form.Control>
+                                    </Form.Group>
+                                    <p>Total: {total}</p>
+                                    <Button className="mt-3" variant="primary" type="submit">Comprar</Button>
+                                </Form>
 
-                        </Container>
-                    </Col>
+                            </Container>
+                        </Col>
+                    </Row>
+                    
+
                 </Container>
             </>
 
