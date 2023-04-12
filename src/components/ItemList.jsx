c6367ec3f92d68d492a170cbd31bc24afc2e40b5
gon2410 <goonolivera@hotmail.com>
2023-04-12 12:05:40 -0300

    some updates

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Item from './Item'
-import Container from 'react-bootstrap/Container'
+
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
